6a3e9adff10d1a7f7f188582384c68c9c666658c
sangeetha2194 <sangeethachem07@gmail.com>
2024-02-22 19:27:45 +0530

    added some changes

--- hello_word.java
+++ hello_word.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! of sgt & venuu and nidhi"); 
+        System.out.println("Hello, World! of sgt & venuu and nidhi ok"); 
     }
 }
